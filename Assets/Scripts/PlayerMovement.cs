112c6532a556b764907396449e0b94a2bc9b73fb
studiocypherislive-CYP <studiocypherislive@gmail.com>
2026-01-02 16:04:32 +0200

    replaced all with simple code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Dynamic;
 using Unity.VisualScripting;
 using UnityEditor.Tilemaps;
 using UnityEngine;
@@ -7,104 +8,81 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 7f;
+    private float movement;
+    public float jumpHeight = 8f;
 
-    public Rigidbody2D rb;
-    public Transform groundCheck;
-    public LayerMask groundLayer;
+    private Rigidbody2D rb;
+    private bool isGround;
 
-    private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 16f;
-    private bool isFacingRight = true;
+    public Transform groundCheckPoint;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask whatIsGround;
 
-    [SerializeField] private Animator animator;
+    public int jump_Count = 2;
+    private int total_Jumps;
 
-    // Animator parameter names
-    private const string isRunningParam = "isRunning";
-    private const string isJumpingParam = "isJumping";
-    private const string isFallingParam = "isFalling";
+    private bool facing_Right;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        // animator adding part
-        if (animator == null)
+        movement = 0f;
+        isGround = true;
+        facing_Right = true;
+        rb = this.gameObject.GetComponent < Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            animator = GetComponent<Animator>();
+            Jump();
+        }
+
+        Collider2D collInfo = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
+
+        if (collInfo == true)
+        {
+            isGround = true;
+        }
+        else
+        {
+            isGround = false;
+        }
+
+        Flip();
+
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += new Vector3(movement, 0f, 0f) * Time.fixedDeltaTime * speed;
+    }
+
+    void Flip()
+    {
+        if (movement < 0f && facing_Right == true)
+        {
+            transform.eulerAngles = new Vector3(0, -180, 0);
+            facing_Right = false;
+        }
+        else if (movement > 0f && facing_Right == false)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            facing_Right = true;
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Jump()
     {
-        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+        if (isGround == true)
+        {
+            rb.linearVelocityY = jumpHeight;
+            isGround = false;
+        }
         
-        UpdateAnimations();
-
-        if (!isFacingRight && horizontal > 0f)
-        {
-            Flip();
-        }
-        else if (isFacingRight && horizontal < 0f)
-        {
-            Flip();
-        }
     }
-
-    // Updating animation parameters based on player state
-    private void UpdateAnimations()
-    {
-        if (animator == null) return;
-
-        bool isGrounded = IsGrounded();
-        bool isMoving = Mathf.Abs(horizontal) > 0.1f;
-        float verticalVelocity = rb.linearVelocity.y;
-
-        // Set isRunning parameter - true when moving horizontally and grounded
-        animator.SetBool(isRunningParam, isMoving && isGrounded);
-
-        // Set isJumping parameter - true when moving upward (positive vertical velocity)
-        animator.SetBool(isJumpingParam, !isGrounded && verticalVelocity > 0.1f);
-
-        // Set isFalling parameter - true when moving downward (negative vertical velocity) and not grounded
-        animator.SetBool(isFallingParam, !isGrounded && verticalVelocity < -0.1f);
-    }
-
-    //Jump system
-    public void Jump(InputAction.CallbackContext context)
-    {
-        if (context.performed && IsGrounded())
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
-        }
-
-        if (context.canceled && rb.linearVelocity.y > 0f)
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
-        }
-    }
-
-    //ground check system
-    private bool IsGrounded()
-    {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-    }
-
-    //flip system
-    private void Flip()
-    {
-        isFacingRight = !isFacingRight;
-        Vector3 localScale = transform.localScale;
-        localScale.x *= -1f;
-        transform.localScale = localScale;
-    }
-
-    //Move system
-    public void Move(InputAction.CallbackContext context)
-    {
-        horizontal = context.ReadValue<Vector2>().x;
-
-    }
-    
 
 }
